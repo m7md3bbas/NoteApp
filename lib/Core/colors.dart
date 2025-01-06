8d307fdf59d2f1a985805315cbfea18386db5cde
M7mdEmadAbbas <mhmdabass330@gmail.com>
2025-01-06 13:42:03 +0200

    added editor body

--- lib/Core/colors.dart
+++ lib/Core/colors.dart
@@ -4,4 +4,5 @@ class MyColors {
   static const Color primaryColor = Color(0xff252525);
   static const Color textColor = Color(0xffFFFFFF);
   static const Color iconBackGroundColor = Color(0xff3B3B3B);
+  static const Color textNoteColor = Color(0xff9A9A9A);
 }
--- lib/Screens/Editor.dart
+++ lib/Screens/Editor.dart
@@ -1,15 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:noteapp/Screens/widgets/custom_icon_button_widget.dart';
+import 'package:noteapp/Core/colors.dart';
+import 'package:noteapp/Screens/widgets/editor_header.dart';
 
 class Editor extends StatelessWidget {
   const Editor({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return Scaffold(
+      backgroundColor: MyColors.primaryColor,
+      body: SafeArea(
+          child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
-      children: [],
-    ));
+          children: [
+            Expanded(child: EditorHeader()),
+            SizedBox(height: 20),
+            Expanded(child: EditorBody()),
+            Spacer()
+          ],
+        ),
+      )),
+    );
   }
 }
 
+class EditorBody extends StatelessWidget {
+  EditorBody({super.key});
+  TextEditingController titleController = TextEditingController();
+  TextEditingController subjectController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextFormField(
+          decoration: InputDecoration(
+              hintText: "Title",
+              hintStyle: TextStyle(fontSize: 48, color: MyColors.textNoteColor),
+              border: InputBorder.none),
+          cursorHeight: 52,
+          controller: titleController,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Expanded(
+          child: TextFormField(
+            expands: true,
+            decoration: InputDecoration(
+                hintText: "Type something...",
+                border: InputBorder.none,
+                constraints: BoxConstraints.expand(),
+                hintStyle: TextStyle(
+                  color: MyColors.textNoteColor,
+                  fontSize: 23,
+                )),
+            cursorHeight: 35,
+            controller: subjectController,
+          ),
+        ),
+      ],
+    );
+  }
+}
