ef94e6266f345682e148bf56ed23524924518b50
M7mdEmadAbbas <mhmdabass330@gmail.com>
2025-01-08 13:50:04 +0200

    added native splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:noteapp/Screens/home.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(const Duration(seconds: 2), () {
+    FlutterNativeSplash.remove();
+  });
   runApp(const NotesApp());
 }
 
@@ -10,6 +16,8 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
+
     return const MaterialApp(debugShowCheckedModeBanner: false, home: Home());
   }
 }
